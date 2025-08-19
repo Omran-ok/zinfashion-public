ecbe98507e8f31d85df24f5fc546813026377e6e
Omran-ok <omrankablawi.ok@gmail.com>
2025-08-19 14:46:01 +0300

    Create api.php

---
+++ public_html/dev_staging/api.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * ZIN Fashion - API Endpoint
+ * Location: /public_html/dev_staging/api.php
+ * 
+ * This file should be accessed directly for API calls:
+ * https://dev.zinfashion.de/api.php?api=products
+ */
+
+// Include configuration
+require_once 'includes/config.php';
+
+// Include API handler functions
+require_once 'includes/api.php';
+
+// The api.php file will handle the request if $_GET['api'] is set
+?>
